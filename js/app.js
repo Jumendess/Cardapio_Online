ba0f5e37b810f4a95cbdacefcc5380e2a8ab3778
Jumendess <julio.mendes60@gmail.com>
2023-09-23 02:10:14 -0300

    Finalizando a responsividade

--- js/app.js
+++ js/app.js
@@ -510,7 +510,7 @@ cardapio.templates = {
       <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidadeCarrinho('\${id}')"><i class="fas fa-minus"></i></span>
       <span class="add-numero-itens" id="qtnd-carrinho-\${id}">\${qtnd}</span>
       <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidadeCarrinho('\${id}')"><i class="fas fa-plus"></i></span>
-        <span class="btn btn-remove" onclick="cardapio.metodos.removerItemCarrinho('\${id}')"><i class="fas fa-times"></i></span>
+        <span class="btn btn-remove no-mobile" onclick="cardapio.metodos.removerItemCarrinho('\${id}')"><i class="fas fa-times"></i></span>
       </div>
     </div>
       `,
